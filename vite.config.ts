15ddcca7e49fac10b1db1ffac217ca67163096b9
DevSlashRichie <ricardo@binarysails.com>
2026-08-08 23:55:43 -0600

    ci(deploysh): added keyword

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
     devtools(),
     tailwindcss(),
     tanstackStart({
       spa: {
         enabled: true,
         prerender: {
-          crawlLinks: true,
+          enabled: false,
         },
       },
     }),
